aea065f3858e5e5d94b3e0a0d8cb275e5115b994
Venorah <name@firma.de>
2011-04-13 17:29:25 +0200

    Copied method from caesar and new Logger method

--- 01_Vigenere/src/task1/Vigenere.java
+++ 01_Vigenere/src/task1/Vigenere.java
@@ -13,55 +13,54 @@ package task1;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 
+import de.tubs.cs.iti.jcrypt.chiffre.CharacterMapping;
 import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
 
 /**
  * Dummy-Klasse für die Vigenère-Chiffre.
- *
+ * 
  * @author Martin Klußmann
  * @version 1.0 - Tue Mar 30 15:53:38 CEST 2010
  */
 public class Vigenere extends Cipher {
 
   /**
-   * Analysiert den durch den Reader <code>ciphertext</code> gegebenen
-   * Chiffretext, bricht die Chiffre bzw. unterstützt das Brechen der Chiffre
-   * (ggf. interaktiv) und schreibt den Klartext mit dem Writer
-   * <code>cleartext</code>.
-   *
+   * Analysiert den durch den Reader <code>ciphertext</code> gegebenen Chiffretext, bricht die
+   * Chiffre bzw. unterstützt das Brechen der Chiffre (ggf. interaktiv) und schreibt den Klartext
+   * mit dem Writer <code>cleartext</code>.
+   * 
    * @param ciphertext
-   * Der Reader, der den Chiffretext liefert.
+   *          Der Reader, der den Chiffretext liefert.
    * @param cleartext
-   * Der Writer, der den Klartext schreiben soll.
+   *          Der Writer, der den Klartext schreiben soll.
    */
   public void breakCipher(BufferedReader ciphertext, BufferedWriter cleartext) {
 
   }
 
   /**
-   * Entschlüsselt den durch den Reader <code>ciphertext</code> gegebenen
-   * Chiffretext und schreibt den Klartext mit dem Writer
-   * <code>cleartext</code>.
-   *
+   * Entschlüsselt den durch den Reader <code>ciphertext</code> gegebenen Chiffretext und schreibt
+   * den Klartext mit dem Writer <code>cleartext</code>.
+   * 
    * @param ciphertext
-   * Der Reader, der den Chiffretext liefert.
+   *          Der Reader, der den Chiffretext liefert.
    * @param cleartext
-   * Der Writer, der den Klartext schreiben soll.
+   *          Der Writer, der den Klartext schreiben soll.
    */
   public void decipher(BufferedReader ciphertext, BufferedWriter cleartext) {
 
   }
 
   /**
-   * Verschlüsselt den durch den Reader <code>cleartext</code> gegebenen
-   * Klartext und schreibt den Chiffretext mit dem Writer
-   * <code>ciphertext</code>.
+   * Verschlüsselt den durch den Reader <code>cleartext</code> gegebenen Klartext und schreibt den
+   * Chiffretext mit dem Writer <code>ciphertext</code>.
    * 
    * @param cleartext
-   * Der Reader, der den Klartext liefert.
+   *          Der Reader, der den Klartext liefert.
    * @param ciphertext
-   * Der Writer, der den Chiffretext schreiben soll.
+   *          Der Writer, der den Chiffretext schreiben soll.
    */
   public void encipher(BufferedReader cleartext, BufferedWriter ciphertext) {
 
@@ -75,14 +74,48 @@ public class Vigenere extends Cipher {
    */
   public void makeKey() {
 
-    System.out.println("Dummy für die Schlüsselerzeugung.");
+    BufferedReader standardInput = launcher.openStandardInput();
+    boolean accepted = false;
+
+    Logger("Geben Sie den Modulus ein: ");
+    try {
+      modulus = Integer.parseInt(standardInput.readLine());
+
+      String defaultAlphabet = CharacterMapping.getDefaultAlphabet(modulus);
+      if (!defaultAlphabet.equals("")) {
+        Logger("Vordefiniertes Alphabet: '" + defaultAlphabet + "'\nDieses vordefinierte Alphabet kann durch Angabe einer " + "geeigneten Alphabet-Datei\nersetzt werden. Weitere " + "Informationen finden Sie im Javadoc der Klasse\n'Character" + "Mapping'.");
+        accepted = true;
+      } else {
+        Logger("Warnung: Dem eingegebenen Modulus kann kein Default-" + "Alphabet zugeordnet werden.\nErstellen Sie zusätzlich zu " + "dieser Schlüssel- eine passende Alphabet-Datei.\nWeitere " + "Informationen finden Sie im Javadoc der Klasse 'Character" + "Mapping'.");
+        accepted = true;
+      }
+    } catch (NumberFormatException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      int shift = Integer.parseInt(standardInput.readLine());
+      if (shift >= 0 && shift < modulus) {
+        accepted = true;
+      } else {
+        Logger("Diese Verschiebung ist nicht geeignet. Bitte " + "korrigieren Sie Ihre Eingabe.");
+      }
+    } catch (NumberFormatException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   /**
    * Liest den Schlüssel mit dem Reader <code>key</code>.
    * 
    * @param key
-   * Der Reader, der aus der Schlüsseldatei liest.
+   *          Der Reader, der aus der Schlüsseldatei liest.
    * @see #makeKey makeKey
    * @see #writeKey writeKey
    */
@@ -94,11 +127,15 @@ public class Vigenere extends Cipher {
    * Schreibt den Schlüssel mit dem Writer <code>key</code>.
    * 
    * @param key
-   * Der Writer, der in die Schlüsseldatei schreibt.
+   *          Der Writer, der in die Schlüsseldatei schreibt.
    * @see #makeKey makeKey
    * @see #readKey readKey
    */
   public void writeKey(BufferedWriter key) {
 
+  }
+
+  private static void Logger(String event) {
+    System.out.println(event);
   }
 }
