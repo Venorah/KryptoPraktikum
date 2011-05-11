b226b869751135bdd060cee5084d3b50be3d5ac6
Dominik <dominik@dominikschuermann.de>
2011-05-11 16:10:36 +0200

    fixes and key for test

--- 03_IDEA/src/task3/IDEA.java
+++ 03_IDEA/src/task3/IDEA.java
@@ -190,10 +190,7 @@ public final class IDEA extends BlockCipher {
    */
   public void readKey(BufferedReader key) {
     try {
-      StringTokenizer st = new StringTokenizer(key.readLine(), " ");
-      
-      keyString = new String();
-      keyString = st.nextToken();
+      String keyString = new String(key.readLine());
 
       Logger("Reading Information: ");
       Logger("+--KeyString: " + keyString);
@@ -221,7 +218,6 @@ public final class IDEA extends BlockCipher {
   public void writeKey(BufferedWriter key) {
     try {
       key.write(keyString);
-      key.newLine();
 
       Logger("Writing Information: ");
       Logger("+--Key: " + keyString);
