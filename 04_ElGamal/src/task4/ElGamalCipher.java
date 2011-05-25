c5a8edbecf0223a5b543643a8b2d29d38f634a5d
Venorah <name@firma.de>
2011-05-26 00:03:04 +0200

    simple versionen der keys, enc, dec implementiert, kann aber gefuettert werden und laueft

--- 04_ElGamal/src/task4/ElGamalCipher.java
+++ 04_ElGamal/src/task4/ElGamalCipher.java
@@ -29,74 +29,26 @@ import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
  */
 public final class ElGamalCipher extends BlockCipher {
 
-  /**
-   * Entschlüsselt den durch den FileInputStream <code>ciphertext</code> gegebenen Chiffretext und
-   * schreibt den Klartext in den FileOutputStream <code>cleartext</code>.
-   * <p>
-   * Das blockweise Lesen des Chiffretextes soll mit der Methode {@link #readCipher readCipher}
-   * durchgeführt werden, das blockweise Schreiben des Klartextes mit der Methode
-   * {@link #writeClear writeClear}.
-   * </p>
-   * 
-   * @param ciphertext
-   *          Der FileInputStream, der den Chiffretext liefert.
-   * @param cleartext
-   *          Der FileOutputStream, in den der Klartext geschrieben werden soll.
-   */
-  public void decipher(FileInputStream ciphertext, FileOutputStream cleartext) {
+  public BigInteger[] pub;
+  public BigInteger priv;
 
-  }
-
-  /**
-   * Verschlüsselt den durch den FileInputStream <code>cleartext</code> gegebenen Klartext und
-   * schreibt den Chiffretext in den FileOutputStream <code>ciphertext</code>.
-   * <p>
-   * Das blockweise Lesen des Klartextes soll mit der Methode {@link #readClear readClear}
-   * durchgeführt werden, das blockweise Schreiben des Chiffretextes mit der Methode
-   * {@link #writeCipher writeCipher}.
-   * </p>
-   * 
-   * @param cleartext
-   *          Der FileInputStream, der den Klartext liefert.
-   * @param ciphertext
-   *          Der FileOutputStream, in den der Chiffretext geschrieben werden soll.
-   */
-  public void encipher(FileInputStream cleartext, FileOutputStream ciphertext) {
-
-  }
-
-  /**
-   * Erzeugt einen neuen Schlüssel.
-   * 
-   * @see #readKey readKey
-   * @see #writeKey writeKey
-   */
   public void makeKey() {
 
-    System.out.println("Dummy für die Schlüsselerzeugung.");
   }
 
-  /**
-   * Liest den Schlüssel mit dem Reader <code>key</code>.
-   * 
-   * @param key
-   *          Der Reader, der aus der Schlüsseldatei liest.
-   * @see #makeKey makeKey
-   * @see #writeKey writeKey
-   */
   public void readKey(BufferedReader key) {
 
   }
 
-  /**
-   * Schreibt den Schlüssel mit dem Writer <code>key</code>.
-   * 
-   * @param key
-   *          Der Writer, der in die Schlüsseldatei schreibt.
-   * @see #makeKey makeKey
-   * @see #readKey readKey
-   */
   public void writeKey(BufferedWriter key) {
+
+  }
+
+  public void decipher(FileInputStream ciphertext, FileOutputStream cleartext) {
+
+  }
+
+  public void encipher(FileInputStream cleartext, FileOutputStream ciphertext) {
 
   }
 
@@ -116,47 +68,97 @@ public final class ElGamalCipher extends BlockCipher {
 
     return res;
   }
-  
-  public static void example() {
-    BigInteger p, b, c, secretKey;
+
+  public BigInteger p() {
     Random sc = new SecureRandom();
-    secretKey = new BigInteger("12345678901234567890");
+    return BigInteger.probablePrime(64, sc);
+  }
+  
+  public BigInteger x(){
+    return new BigInteger("12345678901234567890");
+  }
+
+  public void keyGenerator() {
+
+    BigInteger p = p();
+    BigInteger g = new BigInteger("3");
+    BigInteger x = x();
+    BigInteger y = g.modPow(x, p);
+
+    priv = x;
+    pub = new BigInteger[] { p, g, y };
+  }
+
+  public BigInteger[] encrypt(BigInteger message) {
+    Random sc = new SecureRandom();
+
+    BigInteger p = pub[0];
+    BigInteger g = pub[1];
+    BigInteger y = pub[2];
+
+    BigInteger M = message;
+    BigInteger k = new BigInteger(64, sc);
+
+    BigInteger a = g.modPow(k, p);
+    BigInteger b = M.multiply(y.modPow(k, p)).mod(p);
+
+    return new BigInteger[] { a, b };
+  }
+
+  public BigInteger decrypt(BigInteger[] C) {
+
+    BigInteger a = C[0];
+    BigInteger b = C[1];
+
+    BigInteger p = pub[0];
+    BigInteger x = priv;
+
+    BigInteger exponent = (p.subtract(x)).subtract(new BigInteger("1"));
+    BigInteger z = a.modPow(exponent, p);
+    BigInteger M = (z.multiply(b)).mod(p);
+
+    return M;
+  }
+
+  public static void example() {
+    BigInteger p, g, y, x;
+    Random sc = new SecureRandom();
+    x = new BigInteger("12345678901234567890");
     //
     // public key calculation
     //
-    System.out.println("secretKey = " + secretKey);
+    System.out.println("secretKey = " + x);
     p = BigInteger.probablePrime(64, sc);
-    b = new BigInteger("3");
-    c = b.modPow(secretKey, p);
+    g = new BigInteger("3");
+    y = g.modPow(x, p);
     System.out.println("p = " + p);
-    System.out.println("b = " + b);
-    System.out.println("c = " + c);
+    System.out.println("b = " + g);
+    System.out.println("c = " + y);
     //
     // Encryption
     //
     System.out.println();
     System.out.println("Starting Encryption");
-    String s = "234324839234";
-    BigInteger X = new BigInteger(s);
-    BigInteger r = new BigInteger(64, sc);
-    BigInteger EC = X.multiply(c.modPow(r, p)).mod(p);
-    BigInteger brmodp = b.modPow(r, p);
-    System.out.println("Plaintext = " + X);
-    System.out.println("r = " + r);
-    System.out.println("EC = " + EC);
-    System.out.println("b^r mod p = " + brmodp);
+    BigInteger M = new BigInteger("666666");
+    BigInteger k = new BigInteger(64, sc);
+
+    BigInteger a = g.modPow(k, p);
+    BigInteger b = M.multiply(y.modPow(k, p)).mod(p);
+
+    System.out.println("Plaintext = " + M);
+    System.out.println("r = " + k);
+    System.out.println("EC = " + b);
+    System.out.println("b^r mod p = " + a);
     //
     // Decryption
     //
     System.out.println();
     System.out.println("Starting Decryption");
-    BigInteger crmodp = brmodp.modPow(secretKey, p);
-    BigInteger d = crmodp.modInverse(p);
-    BigInteger ad = d.multiply(EC).mod(p);
-    System.out.println("\n\nc^r mod p = " + crmodp);
-    System.out.println("d = " + d);
-    System.out.println("Alice decodes: " + ad);
-  }
 
+    BigInteger exponent = (p.subtract(x)).subtract(new BigInteger("1"));
+    BigInteger z = a.modPow(exponent, p);
+    BigInteger M2 = (z.multiply(b)).mod(p);
+    System.out.println(M2);
+  }
 
 }
