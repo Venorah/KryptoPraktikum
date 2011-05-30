5b712a0cee3a56a78eda4fc05d673507450bab6e
Venorah <name@firma.de>
2011-05-31 00:08:29 +0200

    key generation

--- 05_Fingerprint/src/task5/Fingerprint.java
+++ 05_Fingerprint/src/task5/Fingerprint.java
@@ -16,7 +16,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
 
+import de.tubs.cs.iti.jcrypt.chiffre.BigIntegerUtil;
 import de.tubs.cs.iti.jcrypt.chiffre.HashFunction;
 
 /**
@@ -26,7 +30,48 @@ import de.tubs.cs.iti.jcrypt.chiffre.HashFunction;
  * @version 1.1 - Sat Apr 03 22:20:18 CEST 2010
  */
 public final class Fingerprint extends HashFunction {
+  
+  public BigInteger p;
+  public BigInteger g1;
+  public BigInteger g2;
 
+  BigInteger ZERO = new BigInteger("0");
+  BigInteger ONE = new BigInteger("1");
+  BigInteger TWO = new BigInteger("2");
+
+  public void getPrimeAndGenerator() {
+    Random sc = new SecureRandom();
+    int k = 512; // prime number with k=512 bits
+    int certainty = 100; // The probability that the new BigInteger represents a prime number will
+                         // exceed (1-1/2^certainty)
+
+    p = null;
+    BigInteger q = null;
+    do {
+      q = new BigInteger(k - 1, certainty, sc);
+      p = q.multiply(BigIntegerUtil.TWO).add(BigIntegerUtil.ONE); // secure prime p = 2q+1
+    } while (!p.isProbablePrime(certainty));
+
+    BigInteger MINUS_ONE = BigInteger.ONE.negate().mod(p); // -1 mod p
+
+    g1 = null;
+    BigInteger factor = null;
+    do {
+      // 2 <= g < p-1
+      g1 = BigIntegerUtil.randomBetween(BigIntegerUtil.TWO, p.subtract(BigIntegerUtil.ONE), sc);
+      factor = g1.modPow(q, p);
+    } while (!factor.equals(MINUS_ONE));
+    
+    g2 = null;
+    BigInteger factor2 = null;
+    do {
+      // 2 <= g < p-1
+      g2 = BigIntegerUtil.randomBetween(BigIntegerUtil.TWO, p.subtract(BigIntegerUtil.ONE), sc);
+      factor2 = g2.modPow(q, p);
+    } while (!factor2.equals(MINUS_ONE));
+  }
+  
+  
   /**
    * Berechnet den Hash-Wert des durch den FileInputStream
    * <code>cleartext</code> gegebenen Klartextes und schreibt das Ergebnis in
@@ -91,4 +136,6 @@ public final class Fingerprint extends HashFunction {
   public void writeParam(BufferedWriter param) {
 
   }
+  
+
 }
