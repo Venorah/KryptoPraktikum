c9c4142f65d9f39afbb155a6f2a9c250db5d9f5d
Dominik <dominik@dominikschuermann.de>
2011-06-08 15:29:44 +0200

    fixed iv

--- Part2/src/com/krypto/idea/IDEA.java
+++ Part2/src/com/krypto/idea/IDEA.java
@@ -57,15 +57,17 @@ public final class IDEA {
     }
 
     // randomly choose 64bit IV
-    Random rnd = new Random();
-    BigInteger iv = new BigInteger(64, rnd);
+    // Random rnd = new Random();
+    // BigInteger iv = new BigInteger(64, rnd);
+    BigInteger iv = new BigInteger("ddc3a8f6c66286d2", 16);
 
     // Cipher Block Chaining (CBC), output as array with 64bit blocks
     BigInteger output[] = cbcLoop(messageArray, iv, true);
 
     // build output for writing to file
     // first prepend iv to String
-    String outputString = iv.toString(16);
+    // String outputString = iv.toString(16);
+    String outputString = new String("");
     // then ciphertext as hex
     for (int i = 0; i < output.length; i++) {
       outputString += output[i].toString(16);
@@ -88,18 +90,28 @@ public final class IDEA {
     // (message.length - 1) because 0 is iv!
     int size = (cipherTextString.length() / 16);
     BigInteger[] messageArray = new BigInteger[(size - 1)];
-    BigInteger iv = null;
+
+    BigInteger iv = new BigInteger("ddc3a8f6c66286d2", 16);
+
     for (int i = 0; i < size; i++) {
       String subString = cipherTextString.substring(0, 16);
       cipherTextString = cipherTextString.substring(16);
 
-      if (i == 0) {
-        iv = new BigInteger(subString, 16);
-
-      } else {
-        messageArray[i - 1] = new BigInteger(subString, 16);
-      }
+      messageArray[i] = new BigInteger(subString, 16);
     }
+
+    // BigInteger iv = null;
+    // for (int i = 0; i < size; i++) {
+    // String subString = cipherTextString.substring(0, 16);
+    // cipherTextString = cipherTextString.substring(16);
+    //
+    // if (i == 0) {
+    // iv = new BigInteger(subString, 16);
+    //
+    // } else {
+    // messageArray[i - 1] = new BigInteger(subString, 16);
+    // }
+    // }
 
     // Cipher Block Chaining (CBC), output as array with 64bit blocks
     BigInteger output[] = cbcLoop(messageArray, iv, false);
